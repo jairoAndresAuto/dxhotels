3bb9c7eeeb94163557f46ddcfdd2f673503813b4
jairoAndresAuto <54287661+jairoAndresAuto@users.noreply.github.com>
2025-03-03 07:31:45 -0500

    Se agrega espera

--- src/main/java/co/com/dxhotels/automatizacion/interactions/Recolectar.java
+++ src/main/java/co/com/dxhotels/automatizacion/interactions/Recolectar.java
@@ -4,12 +4,14 @@ import co.com.dxhotels.automatizacion.util.Constantes;
 import lombok.AllArgsConstructor;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import java.util.List;
 
 import static co.com.dxhotels.automatizacion.userinterfaces.RecolectarDatosUI.LBL_ESTRELLAS;
 import static co.com.dxhotels.automatizacion.userinterfaces.RecolectarDatosUI.LBL_PRECIO;
 import static co.com.dxhotels.automatizacion.util.Constantes.PRECIOS;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 @AllArgsConstructor
 public class Recolectar implements Interaction {
@@ -19,6 +21,7 @@ public class Recolectar implements Interaction {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(WaitUntil.the(LBL_PRECIO,isVisible()));
         precios = LBL_PRECIO.resolveAllFor(actor).texts();
         estrellas= LBL_ESTRELLAS.resolveAllFor(actor).texts();
         actor.remember(PRECIOS, precios);
